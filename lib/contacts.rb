dedf1ed8697ea4368ff37f3970aa8fe8fe7f5fc3
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 02:14:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -39,7 +39,12 @@ contacts.each do |person,contacts_details_hash|
   def remove_strawberry (contacts)
     contacts.each do |person,contacts_details_hash|
       if person == "Freddy Mercury"
-        
+        contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
     
 
 
